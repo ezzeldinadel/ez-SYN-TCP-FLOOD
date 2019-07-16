5e19af861e604b1c12a92b2319e251a35abf4e53
Iman Akbari <imakbari@gmail.com>
2019-07-16 00:32:37 -0400

    No spoof

--- src/dispatch.sh
+++ src/dispatch.sh
@@ -13,6 +13,6 @@ echo Running HTTP server on port $SERVER_PORT
 screen -dmSL server bash $DIR/server.sh $SERVER_PORT
 screen -dmSL stats-report python $DIR/syn-flood-stats.py $TARGET_IP $TARGET_PORT
 
-python $DIR/ezSYN_FLOOD_MULTIPROCESS.py "$TARGET_IP" "$TARGET_PORT" --workers="$NUM_WORKERS" --sleep="$SLEEP_TIME"
+python $DIR/ezSYN_FLOOD_MULTIPROCESS.py "$TARGET_IP" "$TARGET_PORT" --workers="$NUM_WORKERS" --sleep="$SLEEP_TIME" --no-spoof
 
 
